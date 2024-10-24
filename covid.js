260b847fc75eded873808a6b348795b9692a744b
username12121212120beep <mcvv1337@gmail.com>
2024-10-24 20:07:04 -0300

    yes

---
+++ covid.js
@@ -0,0 +1,12 @@
+document.getElementById("geo").onclick = function () {
+    location.href = "geo.html";
+};
+document.getElementById("covid").onclick = function () {
+    location.href = "covid.html";
+};
+document.getElementById("politc").onclick = function () {
+    location.href = "politica.html";
+};
+document.getElementById("steo").onclick = function () {
+    location.href = "index.html";
+};
---
+++ script.js
@@ -0,0 +1,139 @@
+document.getElementById("geo").onclick = function () {
+    location.href = "geo.html";
+};
+document.getElementById("covid").onclick = function () {
+    location.href = "covid.html";
+};
+document.getElementById("politc").onclick = function () {
+    location.href = "politica.html";
+};
+document.getElementById("steo").onclick = function () {
+    location.href = "index.html";
+};
+
+
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "35938b4aa2bfaee2dcbea3b6af01db9b";
+const countryCode = "br";
+const excludeCountry= "";
+
+weatherForm.addEventListener("submit", async event => {
+
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+
+        catch(error){
+
+            console.error(error);
+            displayError(error);
+        }
+
+    }
+
+    else{
+
+        displayError("Please enter a city");
+
+    }
+
+});
+
+
+async function getWeatherData(city){
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+
+    if(!response.ok){
+
+        throw new Error("Could not fetch weather data");
+    }
+
+    return await response.json();
+}
+
+//revisar
+function displayWeatherInfo(data){
+
+    const {name: city, 
+           main: {temp, humidity}, 
+           weather: [{description, id}]} = data;
+
+    //Change (card) style:
+    card.textContent = "";
+    card.style.display = "flex";
+
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const weatherEmoji = document.createElement("p");
+
+    //Info of the text
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
+
+    //Display infos
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+    weatherEmoji.classList.add("weatherEmoji");
+
+    //class CARD 
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
+
+}
+
+function getWeatherEmoji(weatherId){
+
+    switch(true){
+
+        case (weatherId >= 200 && weatherId < 300):
+            return "⛈";
+        case (weatherId >= 300 && weatherId < 400):
+            return "🌧";
+        case (weatherId >= 500 && weatherId < 600):
+            return "🌧";
+        case (weatherId >= 600 && weatherId < 700):
+            return "❄️";
+        case (weatherId >= 700 && weatherId < 800):
+            return "🌫";
+        case (weatherId === 800):
+            return "☀️";
+        case (weatherId >= 801 && weatherId < 810):
+            return "☁️";
+        default:
+            return "❓";
+    }
+}
+
+function displayError(message){
+
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+}
